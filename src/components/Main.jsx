5c8e75dbb0bf7b27ae072b61c016dcbe2f0889f0
Kestutis Masaitis <kmngeru@gmail.com>
2023-06-30 11:04:12 +0300

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+
+const Main = () => {
+    const [filmName, setFilmName] = useState('');
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = (e) => {
+        //1. prevent defaault
+        e.preventDefault()
+
+        console.log(filmName)
+        // 2 ar uzpildyta reiksme
+        if (filmName.length < 3) {
+            alert('filmo pavadinimas turi tureti nemaziau 3 simbolius')
+        } else{ 
+            filmSearchByName(filmName).then(response => {
+                setMovies([...response])
+            })
+        }
+        // 4. uzsetinti i nauja-kita state gauta atsakyma
+    }
+
+    // 3. fetch() i api ideti kintamaji
+    const filmSearchByName = async (filmas) => {
+        try {
+            const result = await fetch(`https://www.omdbapi.com/?apikey=e4db3ced&s=${filmas}`);
+            const data = await result.json()
+            return data.Search
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    console.log(movies)
+
+    return (
+        <div>
+            <form onSubmit={getMovies}>
+                <input
+                    type="text"
+                    name='filmName'
+                    id='filmName'
+                    value={filmName}
+                    onChange={(e) => setFilmName(e.target.value)}
+                />
+                <button>Get movie</button>
+            </form>
+        </div>
+    )
+}
+
+export default Main
